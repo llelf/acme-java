541f812a7c6b038bdcc9d6586ae2c22cff842d3a
Antonio Nikishaev <me@lelf.lu>
2015-12-07 16:37:53 +0000

    simple working ghcj

--- Main.hs
+++ Main.hs
@@ -1,4 +1,26 @@
+import Data.Monoid
+import Control.Applicative
+import System.Process
+
+findJDKIncludes :: IO [FilePath]
+findJDKIncludes = pure [ base, base <> "/darwin" ]
+    where
+      base = "/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/include"
 
 
-main = undefined
+
+jar = "jar"
+ghc = "ghc"
+
+compile = do
+  includes <- (map ("-I"<>)) <$> findJDKIncludes
+  callProcess ghc $ includes <> ["-static", "-fPIC", "start.c", "hello.hs", "-o", "hello.dylib" ]
+
+createJAR = callProcess jar $ ["cfe", "hi.jar", "Main", "Main.class", "hello.dylib" ]
+
+
+
+main = do compile
+          createJAR
+          putStrLn "Done."
 
